b0354f1116e56e0a2c66ebd4f79012584736aba2
killua117 <yashasyogesh117@gmail.com>
2020-12-02 21:21:09 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const https = require('https');
+const path = require('path');
+const http = require('http');
+var jsonParser = bodyparser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyparser.urlencoded({ extended: false })
+
+
+
+app=express();
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/",function(request,response){
+
+    response.sendFile(__dirname+"/index.html");
+});
+app.get("/login",function(request,response){
+
+    response.sendFile(__dirname+"/login.html");
+});
+app.get("/home",function(request,response){
+
+    response.sendFile(__dirname+"/index.html");
+});
+app.get("/aboutme",function(request,response){
+
+    response.sendFile(__dirname+"/aboutme.html");
+});
+
+app.get('/api',jsonParser, function (req, res) {
+
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+})
+
+
+app.listen(9000,function(){
+  console.log("server started on 9000");
+});
