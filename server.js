6c8c3d1fedc5cb6b9a6fdf1f64a738602afa41ab
yogesh-m-2 <yashasyogesh117@gmail.com>
2020-12-03 14:58:15 +0530

    new port

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyparser = require('body-parser');
 const https = require('https');
 const path = require('path');
 const http = require('http');
+var aws = require('aws-sdk');
 var jsonParser = bodyparser.json()
 
 // create application/x-www-form-urlencoded parser
@@ -45,8 +46,12 @@ app.get('/api',jsonParser, function (req, res) {
   res.setHeader('Access-Control-Allow-Credentials', true);
 
 })
+var DB_CONFIG = process.env.DB_CONFIGURATION;
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
 
-const port = process.env.port || 8081;
+const port = process.env.port || 8080;
 app.listen(port,function(){
   console.log(port);
 });
